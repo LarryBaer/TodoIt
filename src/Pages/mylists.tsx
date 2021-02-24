8dcefdf5730802a0e422dc4a416e3053f5f9bf94
Larry Baer <lbaer724@gmail.com>
2021-02-23 21:42:06 -0500

    Initial Commit

---
+++ src/Pages/mylists.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+function MyLists() {
+  return (
+      <div className="MyLists">
+          MyLists
+      </div>
+  );
+}
+
+export default MyLists;
---
+++ src/Pages/statistics.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+function Statistics() {
+    return (
+        <div className="Statistics">
+            Statistics
+        </div>
+    );
+  }
+  
+  export default Statistics;
